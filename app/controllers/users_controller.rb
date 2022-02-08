111a250c2058b7df18e49592b126cacb21c2fa4e
Clementina Mendes <clementinadesmen@gmail.com>
2022-02-08 21:40:05 +0100

    Update Gemfile for Heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,6 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            reset_session
-            log_in @user
             flash[:success] = "Welcome to the Sample App!"
             redirect_to @user
         else
@@ -15,4 +13,12 @@ class UsersController < ApplicationController
             flash[:error] = "Erreur"
         end
     end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_08_110645) do
+ActiveRecord::Schema.define(version: 2022_02_08_142435) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.string "password_confirmation"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
